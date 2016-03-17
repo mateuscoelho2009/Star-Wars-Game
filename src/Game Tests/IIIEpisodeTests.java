b102b5db8bd5f9881337b61175c467917fcd962b
Mateus Menezes Azevedo Coelho <mateuscoelho2009@gmail.com>
2016-03-17 20:40:24 -0300

    Base Character

---
+++ src/Game Tests/IIIEpisodeTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IIIEpisodeTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/III Episode/BaseCharacter.java
+++ src/III Episode/BaseCharacter.java
@@ -1,5 +1,34 @@
-package Common;
+import java.util.Vector;
 
+/*
+ * That is the Character's base class. It will contain the base for
+ * the development of subsequent characters.
+ */
 public abstract class BaseCharacter {
-
+	// Attributes
+	private float[] position,
+		    velocity;
+	
+	private int life,
+				power;
+	
+	BaseCharacter (){
+		life = 0;
+		
+		position = new float[2];
+		position[0] = 0; position[1] = 0;
+		
+		velocity = new float[2];
+		velocity[0] = 0; velocity[1] = 0;
+	}
+	
+	// Methods
+	
+	float[] getPosition() {
+		return position;
+	}
+	
+	float[] getVelocity() {
+		return velocity;
+	}
 }
