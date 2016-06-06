7e2755885de17c71bbe5621c56528c46bc465b41
Mateus Menezes Azevedo Coelho <mateuscoelho2009@gmail.com>
2016-06-06 20:42:53 -0300

    Adaptação

--- src/EvilMario/mouseInput.java
+++ src/EvilMario/mouseInput.java
@@ -1,23 +1,32 @@
 package EvilMario;
 
+import java.awt.Component;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
+
+import IIIEpisode.IIIGameRunner;
 
 public class mouseInput implements MouseListener{
     public void mousePressed(MouseEvent e) {
         int mx = e.getX();
         int my = e.getY();
+        Component frame = null;
 
-        if(mx>=10 && mx<=110) {
-            if(my>=50 && my<=87) {
+        if(my>=570 && my<=600) {
+            if(mx>=10 && mx<=310) {
                 board.State = board.STATE.GAME;
             }
-            if(my>=102 && my<=139) {
-                //HELP
+            if(mx>=325 && mx<=625) {
+				IIIGameRunner.main(new String[0]);
             }
-            if(my>=154 && my<=191) {
-                System.exit(1);
+            if(mx>=645 && mx<=945) {
+            	JOptionPane.showMessageDialog(frame, "Pac");
+            }
+            if(mx>=960 && mx<=1260) {
+            	JOptionPane.showMessageDialog(frame, "Space");
             }
         }
     }
